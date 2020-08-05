d1a2a11abd989abb067b0d768b20b7dc00433f9b
k.kei <acidjannedoor@yahoo.co.jp>
2020-08-05 04:02:23 +0000

    v-calendar実装のため一旦rails側の実装中止

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,8 +1,9 @@
 class AttendancesController < ApplicationController
   
   def index
-    @first_day = Date.current.beginning_of_month
-    @last_day = @first_day.end_of_month
+    #@first_day = Date.current.beginning_of_month
+    #@last_day = @first_day.end_of_month
+    #v-calendar実装のため一旦中止
   end
   
 end
