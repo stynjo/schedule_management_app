f053393c67ce1034722c31ae46aae0f2d382ba1e
k.kei <acidjannedoor@yahoo.co.jp>
2020-09-06 02:01:29 +0000

    reservation_start_time reservation_end_timeカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_06_014834) do
+ActiveRecord::Schema.define(version: 2020_09_06_015552) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2020_09_06_014834) do
     t.integer "number_of_people"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "reservation_start_time"
+    t.datetime "reservation_end_time"
   end
 
   create_table "users", force: :cascade do |t|
