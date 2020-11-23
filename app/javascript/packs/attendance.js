95f6ef8c80e508698d3b8d6e9f4a5d87b245ed0f
k.kei <acidjannedoor@yahoo.co.jp>
2020-11-23 05:01:55 +0000

    bootstrapVue導入

--- app/javascript/packs/attendance.js
+++ app/javascript/packs/attendance.js
@@ -3,7 +3,12 @@ import Vue from 'vue'
 import Attendance from '../attendance.vue'
 import VCalendar from 'v-calendar'
 
+import BootstrapVue from 'bootstrap-vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
 Vue.use(VCalendar);
 
 document.addEventListener('turbolinks:load', () => {
