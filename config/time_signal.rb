5363d6c45ec7c68cc3476ca3a2363874dd84db53
k.kei <acidjannedoor@yahoo.co.jp>
2021-01-03 12:46:07 +0000

    slackへのメッセージ送信実装完了

--- config/time_signal.rb
+++ config/time_signal.rb
@@ -1,7 +1,7 @@
 require "slack"
 
 Slack.configure do |config|
-  config.token = 'xoxb-1591388005351-1618805748065-lYSj82SQgjQPfWqlW28MQQPz'
+  config.token = "xoxb-1591388005351-1618805748065-lYSj82SQgjQPfWqlW28MQQPz"
 end
 
 Slack.chat_postMessage(text: 'TEST!', channel: '#general')
