4e0fac3cb5ea16dddfa965873b0150552aa86bbe
k.kei <acidjannedoor@yahoo.co.jp>
2021-02-21 08:59:10 +0000

    spec/rails_helper.rbにヘルパーを用意する

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,6 +33,9 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  
+  config.include Devise::Test::IntegrationHelpers, type: :request #sign_inヘルパーを提供してくれます
+  config.include FactoryBot::Syntax::Methods #ついでにFactoryBotもincludeしておきます
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
