374149b979a4508e3c4a3504706bc409e94ddaf7
k.kei <acidjannedoor@yahoo.co.jp>
2021-03-11 06:20:00 +0000

    showアクションへのリクエストテスト実装

--- spec/requests/attendance_request_spec.rb
+++ spec/requests/attendance_request_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-# RSpec.describe "attendance", type: :controller do 
-#   describe "GET/show" do
-#     user = FactoryBot.create(:user)
-#       before do
-#         login_user user
-#       end
-#     it "リクエストが成功すること" do
-#       get '/attendances'
-#       expect(response).to have_http_status(200)
-#     end
-#   end
-# end
+RSpec.describe "attendance", type: :request do 
+  describe "GET/show" do
+    let(:user_params) { attributes_for(:user) }
+    before do 
+      post user_session_path, params: { user: user_params }
+    end
+    it "リクエストが成功すること" do
+      get '/attendances'
+      expect(response).to have_http_status(200)
+    end
+  end
+end
