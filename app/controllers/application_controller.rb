3db73d7461aa740f21f12f547c63d413fd80da8d
k.kei <acidjannedoor@yahoo.co.jp>
2021-03-12 06:09:15 +0000

    新規ユーザー作成フォームに、adminを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ protected
 
     def configure_permitted_parameters
       # sign_upのときに、nameをストロングパラメータに追加する
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email,:admin])
       # account_updateのときに、nameをストロングパラメータに追加する
       devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email])
     end
