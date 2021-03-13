0f918f01ba94e052731eb2e46c25583429f437e3
k.kei <acidjannedoor@yahoo.co.jp>
2021-03-13 12:26:30 +0000

    全tsetのリファクタリング

--- spec/factories/attendance.rb
+++ spec/factories/attendance.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     user_id { 1 }
     started_at { 'Mon, 01 Feb 2021 18:00:00 JST +09:00' }
     finished_at { 'Mon, 01 Feb 2021 20:00:00 JST +09:00' }
+    worked_on { '2021-02-01' }
   end
 end
--- spec/requests/devise_users_spec.rb
+++ spec/requests/devise_users_spec.rb
@@ -1,34 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe "UserAuthentications", type: :request do
-  let(:user) { create(:user) }
-  let(:user_params) { attributes_for(:user) }
-  let(:invalid_user_params) { attributes_for(:user, name: "") }
 
   describe 'POST #create' do
     context 'パラメータが妥当な場合' do
       it 'リクエストが成功すること' do
-        post user_registration_path, params: { user: user_params }
-        expect(response.status).to eq 200
+        post user_registration_path, params: { user: FactoryBot.attributes_for(:user) }
+        expect(response.status).to eq 302
       end
 
       it 'createが成功すること' do
-         post user_registration_path, params: { user: user_params }
-         expect(response.status).to eq 200
+         post user_registration_path, params: { user: FactoryBot.attributes_for(:user) }
+         expect(response.status).to eq 302
       end
 
     end
 
     context 'パラメータが不正な場合' do
       it 'リクエストが成功すること' do
-        post user_registration_path, params: { user: invalid_user_params }
+        post user_registration_path, params: { user: FactoryBot.attributes_for(:user, email: nil) }
         expect(response.status).to eq 200
       end
 
 
       it 'createが失敗すること' do
         expect do
-          post user_registration_path, params: { user: invalid_user_params }
+          post user_registration_path, params: { user: FactoryBot.attributes_for(:user, email: nil) }
         end.to_not change(User, :count)
       end
 
