3b38a734cd117178b50f5318fed5266889a20bb3
k.kei <acidjannedoor@yahoo.co.jp>
2021-03-16 14:31:03 +0000

    20200906014834マイグレーションファイルを修正

--- db/migrate/20200906014834_remove_table_name_from_reserves.rb
+++ db/migrate/20200906014834_remove_table_name_from_reserves.rb
@@ -1,6 +1,5 @@
 class RemoveTableNameFromReserves < ActiveRecord::Migration[6.0]
   def change
     remove_column :reserves, :table_name, :string
-    remove_column :reserves, :string, :string
   end
 end
